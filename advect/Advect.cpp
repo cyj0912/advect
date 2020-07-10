cd88c387999ba57abcbefd01da06655ec7e16126
Toby Chen <tobyc@nvidia.com>
2020-07-10 01:02:56 -0700

    Add license header and gitignore

--- advect/Advect.cpp
+++ advect/Advect.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 Toby Chen All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include "App.h"
 #include "BufferQueue.h"
 #include "Renderer.h"
